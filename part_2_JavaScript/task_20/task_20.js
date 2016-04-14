f1664c2e4f1cb04d43f9517715c34b6f553a8813
Sun Weiting <848552669@qq.com>
2016-04-14 09:21:26 +0800

    modified task_20

--- part_2_JavaScript/task_20/task_20.js
+++ part_2_JavaScript/task_20/task_20.js
@@ -1,15 +1,23 @@
-
 var boxWrap = document.getElementById("box-wrap");
+
+var text_area = document.getElementById("input");
+var search_content = document.getElementById("searh_content");
 aqiData = [];
 
-function leftin(){
-
+function leftin(){	
+	var str = text_area.value.trim();
+	var arrWord = str.split(" ");
+	aqiData = arrWord.concat(aqiData);
+	
+	render();
 }
-
-function rightin(){
-
+function rightin(){	
+	var str = text_area.value.trim();
+	var arrWord = str.split(" ");
+	aqiData = aqiData.concat(arrWord);
+	
+	render();
 }
-
 function leftout(){
 	if(aqiData.length === 0){
 		alert("已无数据！");
@@ -28,11 +36,33 @@ function rightout(){
 	render();
 }
 
-function search(){
-
-
+function search(){	
+	render();
+	var searchStr = search_content.value.trim();
+	for(var i=0; i<aqiData.length; i++){
+		var k=0;
+		for(var j=0; j<aqiData[i].length; j++){
+			if(aqiData[i][j] === searchStr[k]){
+				if(aqiData[i].substring(j,j+searchStr.length) === searchStr){
+					boxWrap.children[i].style.color = "black";
+				}else continue;
+			}
+		}
+	}
+	
 }
 
+/*
+*	渲染
+*/
+function render(){
+	boxWrap.innerHTML='';
+	for(var i=0; i<aqiData.length; i++){	
+		var str = "<div>"+aqiData[i]+"</div>";
+
+		boxWrap.innerHTML += str;
+	}
+}
 /*
 *	为按键添加处理函数
 */
@@ -40,33 +70,26 @@ function btnHandle(){
 	var left_in = document.getElementById("leftin");
 	var right_in = document.getElementById("rightin");
 	var left_out = document.getElementById("leftout");
-	var right_in = document.getElementById("rightin");
+	var right_out = document.getElementById("rightout");
 	var search_in = document.getElementById("search");
 
-	left_in.onclick = function()  { leftin()   };
-	right_in.onclick = function() { rightin()  };
-	left_out.onclick = function() { leftout()  };
-	right_out.onclick = function(){ rightout() };
-	search_in.onclick = function(){ search()   };
+	left_in.onclick = function()   { leftin();    };
+	right_in.onclick = function()  { rightin();   };
+	left_out.onclick = function()  { leftout();   };
+	right_out.onclick = function() { rightout();  };	
+	search_in.onclick = function() { search();    };
 }
 
-/*
-*	渲染图表
-*/
-function render(){
-	for(var i=0; i<aqiData.length; i++){
-		var div = document.createElement('div');
-		boxWrap.appendChild(div);
-	}
+function initInput(){
+	text_area.value = "";
+	search_content.value = "";
 }
-
 /*
 *	初始化
 */
-function init(){
+function init(){ 
+	initInput();
 	btnHandle();
-	render();
-	}
 }
 
 init();
