4f9944fd796b213b06e18e28a0ae3331e04233c4
Sun Weiting <848552669@qq.com>
2016-04-26 14:31:42 +0800

    wrote task23

---
+++ task_23/task_23.js
@@ -0,0 +1,97 @@
+var preBtn = document.getElementById("pre");
+var postBtn = document.getElementById("post");
+
+var searchText;
+var timer;
+var array = [];
+
+function treeNode(div){
+	this.div = div;
+	this.chilNode = [];
+}
+
+var root = new treeNode(document.getElementById("treeBox"));
+function buildTree(){	
+	createTreeNode(root);
+}
+function createTreeNode(tNode){
+	var j =0;
+	for(var i =1; i<tNode.div.children.length; i++)
+	{
+		tNode.chilNode[j] = new treeNode(tNode.div.children[i]);
+		createTreeNode(tNode.chilNode[j]);
+		j++;
+	}
+}
+
+/*
+*	遍历
+*/
+function preOrder(parent){
+	array.push(parent);
+	for (var i = 0; i < parent.chilNode.length; i++) {
+		preOrder(parent.chilNode[i]);
+	}
+}
+function postOrder(parent){
+	for (var i = 0; i < parent.chilNode.length; i++) {
+		postOrder(parent.chilNode[i]);
+	}
+	array.push(parent);
+}
+
+/*
+*	查询动画
+*/
+function startSearch(type){
+	clearInterval(timer);
+	initRender();
+
+	var spans = document.getElementsByTagName("span");
+	for(var j=0; j<spans.length; j++){
+		spans[j].style.color = "black";
+	}// 清除上次的查询结果
+
+	searchText = document.getElementById("input").value;
+	var i = 0;
+	array = [];
+
+	switch(type){
+		case "pre": preOrder(root);break;
+		case "post": postOrder(root);break;
+		default: alert("type is wrong");return;
+	}
+	timer = setInterval(function(){
+		initRender();
+		render(i);
+		i++;
+		if(i==array.length){
+			array[i].div.style.backgroundColor = "blue"; 
+			initRender();
+			clearInterval(timer);
+		}
+	},500);
+}
+function render(i){
+	array[i].div.style.backgroundColor = "blue"; 
+	if(searchText == array[i].div.children[0].innerHTML){
+		array[i].div.children[0].style.color = "red";
+	}
+}
+function initRender(){
+	for(var i = 0; i<array.length; i++){
+		array[i].div.style.backgroundColor = "#fff";
+	}
+}
+
+/*
+*	初始化
+*/
+function init(){
+	buildTree();
+
+	preBtn.onclick = function() { startSearch("pre");  };
+	postBtn.onclick = function(){ startSearch("post"); };
+}
+
+init();
